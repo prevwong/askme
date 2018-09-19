1f3fc58db81b69d73304c99f2be01f864ad30564
Prev Wong <prevwong@gmail.com>
2018-09-20 01:30:48 +0800

    :sparkles: default App.js

--- App.js
+++ App.js
@@ -1,29 +1,49 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux'
-import { View, TouchableWithoutFeedback, Keyboard } from "react-native";
-import configureStore from "./src/store";
-import Landing from './src/index';
-const { persistor, store } = configureStore()
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
 type Props = {};
 export default class App extends Component<Props> {
-  async componentDidMount() {
-    console.log("COMPONENT MOUNTED");
-   
-  }
-  onPress() {
-    // setTimreout(() => {
-    Keyboard.dismiss();
-    // }, 200r0);
-  }
-
   render() {
     return (
-      <Provider store={store}>
-        <TouchableWithoutFeedback onPressOut={this.onPress} accessible={false}>
-          <Landing />
-        </TouchableWithoutFeedback>
-      </Provider>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
